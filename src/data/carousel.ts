b9db345f979dfe971f1ee2b23e983dd7ef40c2cb
Doãn Thanh Tuấn <167439120+tuandevdt@users.noreply.github.com>
2025-12-25 22:42:25 +0700

    Feature: add carousel, router & config CI/CD

--- src/data/carousel.ts
+++
@@ -1,6 +0,0 @@
-export const images = [
-  'https://cellphones.com.vn/sforum/wp-content/uploads/2024/02/anh-thien-nhien-4.jpg',
-  'https://img4.thuthuatphanmem.vn/uploads/2019/12/16/hinh-nen-4k-thien-nhien-dep_024352261.jpg',
-  'https://m.yodycdn.com/blog/hinh-nen-thien-nhien-4k-yody-vn-63.jpg',
-  'https://phongvu.vn/cong-nghe/wp-content/uploads/2024/09/130-hinh-nen-may-tinh-4k-19.jpg',
-]
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
 import { createApp } from 'vue'
 import '@/style.css'
 import App from '@/App.vue'
-import router from '@/router'
-import Aura from '@primeuix/themes/aura'
-import PrimeVue from 'primevue/config'
 
-const app = createApp(App)
-
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
-})
-
-app.use(router).mount('#app')
+createApp(App).mount('#app')
