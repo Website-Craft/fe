ee718019be0f5177451c53daed4799ef45d36ea2
tuandevdt <doanthanhtuan1998@gmail.com>
2025-12-25 22:32:14 +0700

    feature: add carousel, router & config CI/CD

---
+++ src/layouts/RootLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen">
+    <layout-header />
+
+    <div class="min-h-[80vh] py-6 px-12"><slot /></div>
+
+    <layout-footer />
+  </div>
+</template>
+<script setup lang="ts">
+import LayoutHeader from '@/components/layout/Header.vue'
+import LayoutFooter from '@/components/layout/Footer.vue'
+defineOptions({
+  name: 'RootLayout',
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import '@/style.css'
 import App from '@/App.vue'
+import router from '@/router'
+import Aura from '@primeuix/themes/aura'
+import PrimeVue from 'primevue/config'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.use(router).mount('#app')
